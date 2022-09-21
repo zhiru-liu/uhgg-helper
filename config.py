d76d46a708c1c5d14d5fcbc4ef68c4f673106cca
Zhiru <zhiru42@gmail.com>
2022-09-21 13:39:03 -0700

    add some path helper

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+# define a bunch of important paths
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+catalog_path = os.path.join(ROOT_DIR, 'dat', '41587_2020_603_MOESM3_ESM.csv')
